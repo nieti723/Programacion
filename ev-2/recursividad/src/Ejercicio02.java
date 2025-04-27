90f228db7fd0bb3518761278b82fa9cc6a1eb7c7
Juan Nieto Acosta <juan.nieto-acosta@iesruizgijon.com>
2025-04-27 11:28:23 +0200

    no tengo ni idea de fibonacci

---
+++ ev-2/recursividad/src/Ejercicio02.java
@@ -0,0 +1,5 @@
+public class Ejercicio02 {
+    public static void main(String[] args) {
+        System.out.println(Funciones.fibonacci(3));
+    }
+}
--- ev-2/recursividad/src/Funciones.java
+++ ev-2/recursividad/src/Funciones.java
@@ -1,10 +1,17 @@
 public class Funciones {
 
-    public static int factorial(int n){
+    public static int factorial(int n) {
         if (n <= 1) {
             return n;
         }
-        return n * factorial(n-1);
+        return n * factorial(n - 1);
+    }
+
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 
 }

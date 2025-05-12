6d9cc9afe9cf95537ffb4b740387f6c3fbc9e9ea
Juan Nieto Acosta <juan.nieto-acosta@iesruizgijon.com>
2025-05-12 09:13:41 +0200

    cambios en ejercicios ficheros

--- ev-2/recursividad/src/Ejercicio05.java
+++ ev-2/recursividad/src/Ejercicio05.java
@@ -1,5 +1,22 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Ejercicio05 {
     public static void main(String[] args) {
-        System.out.println(Funciones.sumaDigitos(88));
+        Scanner s = new Scanner(System.in);
+        int numero = 0;
+        boolean valid;
+        do {
+            valid = true;
+            try {
+                System.out.print("Introduzca un número para sumar sus dígitos: ");
+                numero = s.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("El número debe ser un entero");
+                valid = false;
+                s.nextLine();
+            }
+        } while (!valid);
+        System.out.println(Funciones.sumaDigitos(numero));
     }
 }
